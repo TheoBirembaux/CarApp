9d27d4d9f439cf6f1170ee09a6173d3d787a4c7f
Theo2 <theobirembaux@gmail.com>
2026-03-11 15:00:59 +0100

    Ajout de this pour l'accès au variables

--- src/main/java/Conducteur.java
+++ src/main/java/Conducteur.java
@@ -1,7 +1,7 @@
 public class Conducteur {
     private final String nom;
     private final int age;
-    private final int ageLegale = 10;
+    private static final int AGE_LEGAL = 10;
 
     public Conducteur(final String name, final int years) {
         this.nom = name;
@@ -9,24 +9,24 @@ public class Conducteur {
     }
 
     public boolean estAdulte() {
-        return age >= ageLegale;
+        return AGE_LEGAL <= this.age;
     }
 
     public void demarrerVoiture() {
         final boolean estAdulte = estAdulte();
         if (estAdulte) {
-            System.out.println(nom + " démarre la voiture.");
+            System.out.println(this.nom + " démarre la voiture.");
         } else {
             System.out.println("Le conducteur n'est pas assez âgé pour conduire.");
         }
     }
 
     public void arreterVoiture() {
-        System.out.println(nom + " arrête la voiture.");
+        System.out.println(this.nom + " arrête la voiture.");
     }
 
     public void changerVitesse(final Voiture voiture, final int nouvelleVitesse) {
-        System.out.println(nom + " change la vitesse de la voiture à " + nouvelleVitesse);
+        System.out.println(this.nom + " change la vitesse de la voiture à " + nouvelleVitesse);
         int vitesseActuelle  = voiture.getVitesse();
         if (vitesseActuelle >= nouvelleVitesse) {
             while (vitesseActuelle > nouvelleVitesse) {
